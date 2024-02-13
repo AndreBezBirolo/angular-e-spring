e56056f6a03d6749b67fbbd8107596b1f644d559
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2024-02-13 19:27:54 -0300

    feat: utilização de serviço de validação de enum com java bean

--- crud-spring/src/main/java/com/andre/crudspring/dto/CourseDTO.java
+++ crud-spring/src/main/java/com/andre/crudspring/dto/CourseDTO.java
@@ -1,11 +1,12 @@
 package com.andre.crudspring.dto;
 
+import com.andre.crudspring.enums.Category;
+import com.andre.crudspring.enums.validation.ValueOfEnum;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.Length;
 
 import java.util.List;
@@ -13,6 +14,6 @@ import java.util.List;
 public record CourseDTO(
         @JsonProperty("_id") Long id,
         @NotBlank @NotNull @Length(min = 5, max = 100) String name,
-        @NotNull @Length(max = 25) @Pattern(regexp = "back-end|front-end") String category,
+        @NotNull @Length(max = 25) @ValueOfEnum(enumClass = Category.class) String category,
         @NotNull @NotEmpty @Valid List<LessonDTO> lessons) {
 }
