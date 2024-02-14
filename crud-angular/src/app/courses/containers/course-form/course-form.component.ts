134b525722e6fb1728557f2032b5bd5178e39fd8
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2024-02-13 22:20:29 -0300

    perf: migração para novo control flow

--- crud-angular/src/app/courses/containers/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/containers/course-form/course-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, NonNullableFormBuilder, UntypedFormArray, Validators, FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { CoursesService } from "../../services/courses.service";
-import { Location, NgIf, NgFor } from "@angular/common";
+import { Location } from "@angular/common";
 import { ActivatedRoute } from "@angular/router";
 import { Course } from "../../model/course";
 import { MatSnackBar } from "@angular/material/snack-bar";
@@ -21,7 +21,7 @@ import { MatCard, MatCardContent, MatCardActions } from '@angular/material/card'
     templateUrl: './course-form.component.html',
     styleUrls: ['./course-form.component.scss'],
     standalone: true,
-    imports: [MatCard, MatToolbar, MatCardContent, FormsModule, ReactiveFormsModule, MatFormField, MatInput, MatHint, NgIf, MatError, MatLabel, MatSelect, MatOption, MatIconButton, MatIcon, NgFor, MatPrefix, MatCardActions, MatButton]
+    imports: [MatCard, MatToolbar, MatCardContent, FormsModule, ReactiveFormsModule, MatFormField, MatInput, MatHint, MatError, MatLabel, MatSelect, MatOption, MatIconButton, MatIcon, MatPrefix, MatCardActions, MatButton]
 })
 export class CourseFormComponent implements OnInit {
 
