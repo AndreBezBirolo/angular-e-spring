95e30e20d0a539865b9ead0e6a4e3fcd0c2bbc48
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2024-02-13 21:24:07 -0300

    feat: utilização de docker com mysql

--- crud-spring/src/main/java/com/andre/crudspring/enums/validation/ValueOfEnumValidator.java
+++ crud-spring/src/main/java/com/andre/crudspring/enums/validation/ValueOfEnumValidator.java
@@ -4,7 +4,6 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ValueOfEnumValidator implements ConstraintValidator<ValueOfEnum, CharSequence> {
@@ -13,8 +12,8 @@ public class ValueOfEnumValidator implements ConstraintValidator<ValueOfEnum, Ch
     @Override
     public void initialize(ValueOfEnum annotation) {
         acceptedValues = Stream.of(annotation.enumClass().getEnumConstants())
-                .map(Enum::name)
-                .collect(Collectors.toList());
+                .map(Enum::toString)
+                .toList();
     }
 
     @Override
